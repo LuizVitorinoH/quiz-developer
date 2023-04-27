0c52637b6f943bc12cb395914f275f36bd127df9
Luiz Henrique Vitorino <luizvitorinohenrique@gmail.com>
2023-04-26 22:31:37 -0300

    adjust: app quiz

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,9 +1,24 @@
-import "./app.css";
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className="App">
-      <h1>Quiz App</h1>
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   );
 }
